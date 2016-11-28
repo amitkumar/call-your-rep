5524c3ee24c1fea1e1784c65f114c47518f317f6
Amit Kumar <email@amitkumar.com>
2016-11-27 21:13:50 -0500

    first stabs at twilio integration

--- config/config.js
+++ config/config.js
@@ -14,6 +14,12 @@ var facebook = {
   }
 };
 
+var twilio = {
+  accountSid : 'AC225c052dd5707e5a8458137b2630b9b2',
+  authToken : "d847ddc0c2d54617c00bf304a0043f91",
+  phoneNumber : '+14243292581 '
+}
+
 
 var config = {
   development: {
@@ -25,7 +31,8 @@ var config = {
     url : 'http://localhost:3000',
     db: 'mongodb://admin:jDKW!WPm6R$2@ds163377.mlab.com:63377/call-your-lawmakers',
     facebook : facebook['call-your-lawmakers-test1'],
-    googleMapsKey : 'AIzaSyCFzuCB3XMJbKxJmc_7_m6M49rwHbl2r8s'
+    googleMapsKey : 'AIzaSyCFzuCB3XMJbKxJmc_7_m6M49rwHbl2r8s',
+    twilio : twilio
   },
 
   test: {
@@ -52,7 +59,8 @@ var config = {
       version    : 'v2.6',
       appSecret  : '55e15e4adcc2d34ac4aba0648cc6a247',
       accountKitVersion : 'v1.1'
-    }
+    },
+    twilio : twilio
   }
 };
 
